104bf5fbee7383a5d74b8e3e1c0de514605baae4
pat53 <57118267+pat53@users.noreply.github.com>
2020-09-20 15:12:59 -0600

    Insertion sort and pytest

---
+++ test_sort.py
@@ -0,0 +1,14 @@
+""""
+Pytest tests for insertion sort from main.py
+"""
+from main import *
+
+
+def test_insertion_sort():
+    unsorted_arr = [1, 5, 7, 3, 2]
+    insertionSort(unsorted_arr)
+    assert unsorted_arr == [1, 2, 3, 5, 7]
+
+
+if __name__ == "__main__":
+    test_insertion_sort()
