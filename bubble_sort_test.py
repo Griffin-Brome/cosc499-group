c0d045b2c225db785ab9f699d3657461058fcb4e
Kaze <renyuan7777@gmail.com>
2020-09-21 10:20:47 -0700

    Updated to use snake case

---
+++ bubble_sort_test.py
@@ -0,0 +1,11 @@
+import unittest
+from bubble_sort import bubble_sort
+
+
+def test_bubble_sort():
+	x=[9,8,7,6,5,4,3,2,1]
+	
+	assert(bubble_sort(x)==[1,2,3,4,5,6,7,8,9])
+	print("Test Complete")
+
+test_bubble_sort()
