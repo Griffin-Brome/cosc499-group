ea0e85a6b7954012a3bd3c9a69a8531b183bd3f7
pat53 <57118267+pat53@users.noreply.github.com>
2020-09-21 12:39:46 -0600

    Update for snake code

--- test_selection_sort.py
+++ test_selection_sort.py
@@ -2,6 +2,6 @@ from selection_sort import *
 
 
 def test_selection_sort():
-    unsorted_list = [9,4,5,1,2,13,28,31]
-    sorted_list = [1,2,4,5,9,13,28,31]
+    unsorted_list = [9, 4, 5, 1, 2, 13, 28, 31]
+    sorted_list = [1, 2, 4, 5, 9, 13, 28, 31]
     assert selection_sort(unsorted_list) == sorted_list
