702b462b952757561f41f3d1b469d18aca131ec0
Kaze <renyuan7777@gmail.com>
2020-09-20 22:38:38 -0700

    Standardized Test

--- bubbleSortTest.py
+++ bubbleSortTest.py
@@ -1,5 +1,8 @@
 import unittest
 from bubbleSort import bubbleSort
-x=[9,8,7,6,5,4,3,2,1]
-#print(bubbleSort(x))
-assert(bubbleSort(x)==[1,2,3,4,5,6,7,8,9])
+
+
+def test_bubble_sort():
+	x=[9,8,7,6,5,4,3,2,1]
+	#print(bubbleSort(x))
+	assert(bubbleSort(x)==[1,2,3,4,5,6,7,8,9])
